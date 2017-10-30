fdc6881cd397509e56759e58c9c3672d01f22fbd
David Sieber <david.sieber@gmx.net>
2017-10-31 00:30:53 +0100

    added some crap to get it working

--- praktikumAufgabe04/Program.cs
+++ praktikumAufgabe04/Program.cs
@@ -10,6 +10,63 @@ namespace praktikumAufgabe04
     {
         static void Main(string[] args)
         {
+            const int spielTage = 1000;
+            const int limit = 500;
+            const int startGuthaben = 200;
+            const int startEinsatz = 5;
+            string meineFarbe = "schwarz";
+
+            int guthaben;
+            int countingSpieltag;
+            int gesamtGewinn = 0;
+            string farbe;
+
+            // Zufallszahlengenerator initialisieren
+            Random zufall = new Random();
+            // Kugel werfen: Zufallszahl zwischen 0 und 36 bestimmen
+
+            for (countingSpieltag = 1; countingSpieltag <= spielTage; countingSpieltag++)
+            {
+                Console.WriteLine("----------------------------------------------------------");
+                Console.WriteLine("Spieltag " + countingSpieltag);
+                Console.WriteLine("----------------------------------------------------------");
+
+                guthaben = startGuthaben;
+
+                while (guthaben > 5 && guthaben < 500)
+                {
+
+                    int zahl = zufall.Next(0, 37);
+                    // Farbe rot oder schwarz bestimmen
+
+                    if (zahl == 0)
+                        farbe = "grün";
+                    else if (zahl % 2 == 1)
+                        farbe = "rot";
+                    else
+                        farbe = "schwarz";
+
+                    // Überprüfen ob Farbe richtig gewählt und guthaben anpassen
+                    if (farbe == meineFarbe)
+                        guthaben += (startEinsatz * 2);
+                    else
+                        guthaben -= startEinsatz;
+
+
+
+                    // Ergebnis ausgeben
+                    Console.WriteLine("Gefallen ist {0}, {1}.", zahl, farbe);
+                    Console.WriteLine("Neues Guthaben: " + guthaben);
+
+                }
+
+                Console.WriteLine("Ergebnis Spieltag {0}: {1}", countingSpieltag, (guthaben - 200));
+
+                gesamtGewinn += (guthaben - 200);
+
+            }
+            Console.WriteLine("Gesamtgewinn: " + gesamtGewinn);
+            Console.ReadLine();
         }
     }
 }
